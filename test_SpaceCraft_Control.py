2440c37ed0ac8fad3ff9b4bc0d34099e649c63ae
[Vivek Panchal] <[vivek2003panchal@gmail.com]>
2023-08-29 23:11:26 +0530

    test2 check for input type

--- test_SpaceCraft_Control.py
+++ test_SpaceCraft_Control.py
@@ -1,11 +1,17 @@
 import unittest
+import string
 
 class controlTest(unittest.TestCase):
     def setUp(self):
-        self.inputCommands = 1
+        self.inputCommands = ['f', 'r', 'u', 'b','l','d']
         
     def test_inputExists(self):
         self.assertIsNotNone(self.inputCommands)
+    
+    def test_inputType(self):
+        for i in self.inputCommands:
+            self.assertIn(i,"fblrud")
+        self.assertIsInstance(self.inputCommands,list)
 
 if __name__ == "__main__":
     unittest.main()
